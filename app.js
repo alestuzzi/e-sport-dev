cf4fa22b8c39148a8114e4d1a9867182caa12186
Adrien Lestuzzi <adrien.lestuzzi@heig-vd.ch>
2019-10-10 11:40:07 +0200

    instalation initiale de mongoose

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/esport');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');

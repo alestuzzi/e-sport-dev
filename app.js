0285d80310fd810544a8d6af9c5cbb126e65954a
Lucien Pochon <lucien.pochon@heig-vd.ch>
2019-11-05 08:48:11 +0100

    GET_ALL_POST_AND_GET_ID

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/player', playerRouter);
+app.use('/api/team', teamRouter);
+app.use('/api/tournament', tournamentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/players.js
+++ routes/players.js
@@ -1,22 +1,40 @@
 var express = require('express');
 
+
 const playerRouter = express.Router();
+const Player = require('../models/user');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 
-
+/* GET users listing by lastname */
 playerRouter.get('/', function (req, res, next) {
-
-  
+  Player.find().sort('lastName').exec(function(err, users) {
+    if (err) {
+      return next(err);
+    }
+    res.send(users);
+  });
 });
 
+
+/* POST one player */
 playerRouter.post('/', function (req, res, next) {
 
+  new Player(req.body).save(function (err, savedPlayer) {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(savedPlayer);
+  });
+});
   
+/* GET one player by id */
+playerRouter.get('/:id', loadPlayerFromParamsMiddleware, function (req, res, next) {
+
+  res.send(req.player);
 });
 
-playerRouter.get('/:id', function (req, res, next) {
-
-  
-});
 
 playerRouter.patch('/:id', function (req, res, next) {
 
@@ -27,6 +45,37 @@ playerRouter.delete('/:id', function (req, res, next) {
 
   
 });
+
+
+/* FUNCTIONS*/ 
+
+
+/* catch the id and check it */
+function loadPlayerFromParamsMiddleware(req, res, next) {
+
+  const playerId = req.params.id;
+  if (!ObjectId.isValid(playerId)) {
+    return playerNotFound(res, playerId);
+  }
+
+  let query = Player.findById(playerId)
+
+  query.exec(function (err, player) {
+    if (err) {
+      return next(err);
+    } else if (!player) {
+      return playerNotFound(res, playerId);
+    }
+
+    req.player = player;
+    next();
+  });
+}
+
+/* send an error message if the id is not found */
+function PlayerNotFound(res, playerId) {
+  return res.status(404).type('text').send(`No player found with that ID ${playerId}`);
+}
 
 
 module.exports = playerRouter;

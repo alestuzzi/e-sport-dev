d5e08d8e58b7faae7c13336555fc5e9ef4031e78
Elisa Biver <elisa.biver@heig-vd.com>
2019-11-07 13:57:38 +0100

    ajout de la pagination player

---
+++ routes/utils.js
@@ -0,0 +1,23 @@
+/**
+ * Parses the pagination parameters (i.e. page & page size) from the request.
+ *
+ * @param {ExpressRequest} req - The Express request object
+ * @returns An object with "page" and "pageSize" properties
+ */
+exports.getPaginationParameters = function(req) {
+
+  // Parse the "page" URL query parameter indicating the index of the first element that should be in the response
+  let page = parseInt(req.query.page, 10);
+  if (isNaN(page) || page < 1) {
+    page = 1;
+  }
+
+  // Parse the "pageSize" URL query parameter indicating how many elements should be in the response
+  let pageSize = parseInt(req.query.pageSize, 10);
+  if (isNaN(pageSize) || pageSize < 0 || pageSize > 100) {
+    pageSize = 100;
+  }
+
+  return { page, pageSize };
+};
+
